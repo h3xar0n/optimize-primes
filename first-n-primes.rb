fae40554ae619eafea63c22fd8aae3eb1158bf65
h3xar0n <aron@glambyadelina.com>
2017-04-16 22:11:01 -0400

    turned if and unless into single line control structures

--- first-n-primes.rb
+++ first-n-primes.rb
@@ -3,13 +3,9 @@ require 'prime'
 
 def first_n_primes(n)
 
-  unless n.is_a? Integer
-    return "n must be an integer."
-  end
+  "n must be an integer." unless n.is_a? Integer
 
-  if n <= 0
-    return "n must be greater than 0."
-  end
+  "n must be greater than 0." if n <= 0
   
   prime_array ||= []
   
